d85664de710061b29e5280fbda5fa62a4245aa16
turkelwebsite-code <turkelwebsite@gmail.com>
2025-09-06 15:45:47 +0300

    feat: MODERN RESPONSIVE - Fluid design

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,10 +36,7 @@ export default {
         'md': '768px',
         'lg': '1024px',
         'xl': '1280px',
-        '2xl': '1536px',
-        '3xl': '1920px',
-        '4xl': '2560px',
-        '5xl': '3456px'
+        '2xl': '1536px'
       },
       borderRadius: {
         'sm': '15px',
